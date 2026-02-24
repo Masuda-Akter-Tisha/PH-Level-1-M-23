69940e37e9d730943d4feb4ae3aa6f3de8293e91
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-24 15:32:27 +0600

    added onclick event to various way

--- js/events.js
+++ js/events.js
@@ -1,1 +1,33 @@
 console.log('external file');
+   //option 2 for click event handler 
+        function makeYellow () {
+            document.body.style.backgroundColor = 'yellow';
+        }
+        function makePink () {
+            document.body.style.backgroundColor = 'pink';
+        }
+    
+        //3rd option get element by id and set onclick
+        const btnMakeBlue = document.getElementById ('btn-make-blue');
+        btnMakeBlue.onclick = function makeBlue () {
+        document.body.style.backgroundColor = 'blue';
+       }
+       //same same but different
+       const btnMakePurple = document.getElementById ('btn-make-purple');
+        btnMakePurple.onclick = makePurple;
+        function makePurple () {
+            document.body.style.backgroundColor = 'purple';
+        }
+
+        //option 4: addEventListener () 
+        // document.getElementById().addEventListener('event type',handler)
+        document.getElementById ('btn-make-skyblue').addEventListener ('click',function makeSkyBlue () {
+            document.body.style.backgroundColor = 'skyBlue'
+        })
+
+        //same same but different
+        const btnMakeOrange = document.getElementById ('btn-make-orange');
+        btnMakeOrange.addEventListener ('click',makeOrange)
+        function makeOrange () {
+            document.body.style.backgroundColor = 'orange';
+        }
