9365407858c72056867715f47f2507d3691c041b
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-26 22:37:24 +0600

    added mouse event

---
+++ js/more-events.js
@@ -0,0 +1,28 @@
+// const btnMouse = document.getElementById ('btn-mouse');
+// btnMouse.addEventListener ('mouseenter', function () {
+//     console.log('event triggered');
+// })
+
+// const btnMouse = document.getElementById ('btn-mouse');
+// btnMouse.addEventListener ('mousedown', function () {
+//     console.log('event triggered');
+// })
+
+// const btnMouse = document.getElementById ('btn-mouse');
+// btnMouse.addEventListener ('mouseleave', function () {
+//     console.log('event triggered');
+// })
+
+// const btnMouse = document.getElementById ('btn-mouse');
+// btnMouse.addEventListener ('mousemove', function () {
+//     console.log('event triggered');
+// })
+
+// const btnMouse = document.getElementById ('btn-mouse');
+// btnMouse.addEventListener ('mouseover', function () {
+//     console.log('event triggered');
+// })
+const btnMouse = document.getElementById ('btn-mouse');
+btnMouse.addEventListener ('mouseup', function () {
+    console.log('event triggered');
+})
