1f620e556df6a44d97730f03f64c3c53e4045b80
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-28 14:51:49 +0600

    added key event

--- js/more-events.js
+++ js/more-events.js
@@ -1,3 +1,4 @@
+//mouse event
 // const btnMouse = document.getElementById ('btn-mouse');
 // btnMouse.addEventListener ('mouseenter', function () {
 //     console.log('event triggered');
@@ -25,4 +26,27 @@
 const btnMouse = document.getElementById ('btn-mouse');
 btnMouse.addEventListener ('mouseup', function () {
     console.log('event triggered');
+})
+
+// form event
+// const userName = document.getElementById ('user-name');
+// userName.addEventListener ('focus', function () {
+//     console.log('user about to write name');
+// })
+// const userEmail = document.getElementById ('user-email');
+// userEmail.addEventListener ('focus', function () {
+//     console.log('user about to write email');
+// })
+// const userName1 = document.getElementById ('user-name');
+// userName.addEventListener ('blur', function () {
+//     console.log('user done with name');
+// })
+// const userEmail2 = document.getElementById ('user-email');
+// userEmail.addEventListener ('blur', function () {
+//     console.log('user done with email');
+// })
+//key event
+const userEmail2 = document.getElementById ('user-email');
+userEmail2.addEventListener ('keydown', function (event) {
+    console.log('typing',event);
 })
